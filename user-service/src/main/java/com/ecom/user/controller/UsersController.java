4af661d3177d184313f6240b3137def4f9d81079
nikhilkumar33 <nikhilkumarjena33@gmail.com>
2026-01-19 17:47:31 +0530

    added config server

--- user-service/src/main/java/com/ecom/user/controller/UsersController.java
+++ user-service/src/main/java/com/ecom/user/controller/UsersController.java
@@ -1,19 +1,25 @@
 package com.ecom.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.user.request.UsersRequest;
+import com.ecom.user.service.UsersService;
 
 @RestController
-public class UsersController {
-
+public class UsersController 
+{
+	@Autowired
+	UsersService usersService;
 	
 	@PostMapping("/users")
-	public ResponseEntity createUser(@RequestBody UsersRequest usersRequest)
+	public ResponseEntity<String> createUser(@RequestBody UsersRequest usersRequest)
 	{
-		return ResponseEntity.ok(null);
+		long userId = usersService.createUser(usersRequest);
+		
+		return ResponseEntity.ok("Registation successfully done. Your user id is: "+userId);
 	}
 }
