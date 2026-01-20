5691dc52909032452280a3e84bca24db2a814ce9
nikhilkumar33 <nikhilkumarjena33@gmail.com>
2026-01-20 14:40:54 +0530

    addedd payment service

---
+++ payment-service/src/main/java/com/ecom/payment/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.ecom.payment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.payment.request.PaymentRequest;
+import com.ecom.payment.response.PaymentResponse;
+import com.ecom.payment.service.PaymentService;
+
+@RestController
+public class PaymentController {
+
+	@Autowired
+	@Qualifier("upi")
+	PaymentService paymentService;
+	
+	@PostMapping("/makePayment")
+	public ResponseEntity<PaymentResponse> makePayment(@RequestBody PaymentRequest paymentRequest) {
+
+		PaymentResponse response = paymentService.processsPayment(paymentRequest);
+
+		return ResponseEntity.ok(response);
+	}
+}
---
+++ payment-service/src/main/java/com/ecom/payment/request/PaymentRequest.java
@@ -0,0 +1,19 @@
+package com.ecom.payment.request;
+
+public class PaymentRequest {
+	private long orderId;
+	private int amount;
+	
+	public long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
