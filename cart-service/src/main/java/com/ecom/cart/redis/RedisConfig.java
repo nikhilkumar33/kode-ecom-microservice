bbced09ee75198179a95686d8beeb939b3cf135d
nikhilkumar33 <nikhilkumarjena33@gmail.com>
2026-02-13 16:05:36 +0530

    docker file added

--- cart-service/src/main/java/com/ecom/cart/redis/RedisConfig.java
+++ cart-service/src/main/java/com/ecom/cart/redis/RedisConfig.java
@@ -1,20 +1,10 @@
 package com.ecom.cart.redis;
 
-import java.time.Duration;
-
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
